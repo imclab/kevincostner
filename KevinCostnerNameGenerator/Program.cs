b07268c02414693f58c6dd434d9b3f64e0a2c4e5
Markus Vuorio <markus.vuorio@gmail.com>
2014-01-08 09:24:39 +0200

    Initial commit with alpha software

---
+++ KevinCostnerNameGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KevinCostnerNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kevin Costner Name Generator v0.1");
+            Console.WriteLine("© 2014 CO-SKY Project, University of Jyväskylä");
+            Console.WriteLine("Generated name: Kevin Costner");
+        }
+    }
+}
